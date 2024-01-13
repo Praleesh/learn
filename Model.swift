d79cd8305b105128cae4ae3dc3e60dae85255f33
praleeshpp <praleeshpradeep@gmail.com>
2024-01-13 11:54:25 +0530

    Add files via upload

---
+++ Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  ApiTutorial
+//
+//  Created by FAO on 09/06/23.
+//
+
+import Foundation
+
+struct DataModel: Codable{
+    
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ApiTutorial
+//
+//  Created by FAO on 09/06/23.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
